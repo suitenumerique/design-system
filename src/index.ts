e43fd84bd4ce8eef4ce17645811bf9cddc79bf68
Nathan Panchout <nathanpanchout@hotmail.com>
2025-02-28 17:56:12 +0100

    ✨(components) add Language, Loader, Separator, and other components
    - Implemented LanguagePicker component with dropdown functionality
    - Created Spinner component with multiple size variants
    - Added HorizontalSeparator component with optional padding
    - Updated index.ts and library.scss to export and include new components
    - Added Storybook stories for new components

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import "./library.scss";
 
 export * from "./components/button";
 export * from "./components/dropdown-menu";
+export * from "./components/language";
+export * from "./components/loader";
 export * from "./components/form";
+export * from "./components/separator";
 
 export { default as cunninghamConfig } from "../cunningham";
