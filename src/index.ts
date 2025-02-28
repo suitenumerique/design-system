1d51f65c4c0bdbefde58ded5390bec288097df22
Nathan Panchout <nathanpanchout@hotmail.com>
2025-02-28 17:54:12 +0100

    ✨(components) add Checkbox, Radio, Switch, and Label
    - Implemented Form components: Checkbox, Radio, Switch, and WithLabel
    - Added comprehensive Storybook stories for each component
    - Created SCSS styles for form components with various states and configurations
    - Exported new form-related components and styles
    - Updated library.scss and index.ts to include new form components

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import "./library.scss";
 
 export * from "./components/button";
 export * from "./components/dropdown-menu";
+export * from "./components/form";
 
 export { default as cunninghamConfig } from "../cunningham";
